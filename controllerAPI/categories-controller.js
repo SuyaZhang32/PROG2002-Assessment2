c97d0e365781290970dbf2d9f5d20feb58a62015
SuyaZhang32 <1797443090@qq.com>
2025-09-26 16:51:54 +0800

    create category API

---
+++ controllerAPI/categories-controller.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../db');
+
+const conn = db.getconnection();
+conn.connect();
+
+router.get('/', (req, res, next) => {
+  const conn = db.getconnection();
+  conn.query('SELECT id, name FROM categories ORDER BY name ASC', (err, rows) => {
+    conn.end();
+    if (err) return next(err);
+    res.json(rows);
+  });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
-const concertAPI = require('./controllerAPI/api-controller');
+const eventAPI = require('./controllerAPI/events-controller');
+const categoryAPI = require('./controllerAPI/categories-controller');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/api/concerts', concertAPI);
+app.use('/api/categories', categoryAPI);
+app.use('/api/events', eventAPI);
 
 app.listen(3060, () => console.log('Server up on 3060'));
